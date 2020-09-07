1009963ce0eef38a90ad4afffd4251ce7aecf332
ma22npk <gunjo64@icloud.com>
2020-09-08 00:59:10 +0900

    phpファイルにキーの入力

--- task/sig.php
+++ task/sig.php
@@ -5,9 +5,9 @@ $alg=@$_POST['alg'];
 if($alg==NULL||!in_array($alg, $alg_arr)){
         $alg='sha1';
 }
-$appKey = "";
-$secretKey = "";
-$timestamp=floor(microtime(1)*1000); 
+$appKey = "1575874525000026";
+$secretKey = "8c80157c9bcc5b3187af1e4775e6a6b1";
+$timestamp=floor(microtime(1)*1000); //タイムスタンプを設定する。（形式例：111111111111111111）
 $rs=json_encode(array('timestamp'=>(string)$timestamp,'sig'=>hash($alg,$appKey . $timestamp . $secretKey)));
-die($rs);
+die($rs);//暗号化処理したタイムスタンプとアルゴリズムを処理した署名文字列をjsonデータ形式に変換する
 ?>
